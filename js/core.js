bc7ce934ea15a2aac330c64ebf6bbd107448be24
Yakimishin <yakimishinruslan@yandex.ru>
2022-07-06 19:51:55 +0300

    add interactive form

--- js/core.js
+++ js/core.js
@@ -2,15 +2,45 @@ const login = document.querySelector('.link-login');
 const loginPopup = document.querySelector('.modal-login');
 const closeLoginPopup = loginPopup.querySelector('.modal-close');
 const innerLogin = loginPopup.querySelector('#inner-login');
+const innerPassword = loginPopup.querySelector('#inner-password');
 
 const mapModal = document.querySelector('.modal-map');
 const mapClose = mapModal.querySelector('.modal-close');
 const howFind = document.querySelector('.contacts-button-map');
 
+const modals = document.querySelectorAll('.modal');
+const loginForm = loginPopup.querySelector('form');
+
+
+
+// ______________________________________________________Проверяем работает ли Local Storage на данном устройстве или браузере.
+
+let isStorageSupport = true;
+let storage = '';
+
+try{
+	storage = localStorage.getItem('login');
+}catch (err){
+	isStorageSupport = false;
+}
+
+// ______________________________________________________________________________________________________
+
+
+
+
+
 login.addEventListener('click', function(evt){
 	evt.preventDefault();
 	loginPopup.classList.remove('hidden');
 	innerLogin.focus();
+	if(storage){
+		innerLogin.value = storage;
+		innerPassword.focus();
+	}else{
+		innerLogin.focus();
+	}
+	
 });
 
 closeLoginPopup.addEventListener('click', function(){
@@ -26,3 +56,22 @@ howFind.addEventListener('click', function(evt){
 mapClose.addEventListener('click', function(){
 	mapModal.classList.add('hidden');
 });
+
+loginForm.addEventListener('submit', function(evt){
+	if(!innerLogin.value || !innerPassword.value){
+		evt.preventDefault();
+		console.log('Заполните форму!');
+	}else{
+		if(isStorageSupport){
+			localStorage.setItem('login',innerlogin.value);
+		}
+	}
+})
+
+window.addEventListener('keydown', function(evt){
+	if(evt.keyCode === 27){
+		for(i=0; i<modals.length; i++){
+			modals[i].classList.add('hidden');
+		}
+	}
+})
